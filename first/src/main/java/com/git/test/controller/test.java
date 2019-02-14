525531c1fae52cd26fcb2df726d02effb39a9855
glo <glo@qq.com>
2019-02-14 17:09:52 +0800

    17/09 commit

--- first/src/main/java/com/git/test/controller/test.java
+++ first/src/main/java/com/git/test/controller/test.java
@@ -3,7 +3,7 @@ package com.git.test.controller;
 public class test {
 
     public static void main(String[] args){
-        System.out.println("git repository identify---");
+        System.out.println("git repository identify-new--");
     }
 
 }
